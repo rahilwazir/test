e84faa6ec201cd91c6ffc71265b6430ede483a9e
rahilwazir <rahilwazir@hotmail.com>
2014-09-15 21:28:20 +0500

    papi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 (function(ng) {
   'use strict';
   
-  var app = ng.module('motatami', []);
+  var app = ng.module('motatami', ['papi']);
   
   app.controller('motatamiCTRL', ['$scope', function($scope) {
     $scope.data = [1, 2, 3, 4];
